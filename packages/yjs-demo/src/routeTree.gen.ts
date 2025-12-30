4b526e8d4e5bfa3249aba7d625a242d7cd320032
Valter Balegas <balegas@electric-sql.com>
2025-12-30 12:44:14 +0100

    style: fix prettier formatting in generated files

--- packages/yjs-demo/src/routeTree.gen.ts
+++ packages/yjs-demo/src/routeTree.gen.ts
@@ -8,40 +8,40 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root'
-import { Route as IndexRouteImport } from './routes/index'
-import { Route as RoomRoomIdRouteImport } from './routes/room.$roomId'
+import { Route as rootRouteImport } from "./routes/__root"
+import { Route as IndexRouteImport } from "./routes/index"
+import { Route as RoomRoomIdRouteImport } from "./routes/room.$roomId"
 
 const IndexRoute = IndexRouteImport.update({
-  id: '/',
-  path: '/',
+  id: "/",
+  path: "/",
   getParentRoute: () => rootRouteImport,
 } as any)
 const RoomRoomIdRoute = RoomRoomIdRouteImport.update({
-  id: '/room/$roomId',
-  path: '/room/$roomId',
+  id: "/room/$roomId",
+  path: "/room/$roomId",
   getParentRoute: () => rootRouteImport,
 } as any)
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '/room/$roomId': typeof RoomRoomIdRoute
+  "/": typeof IndexRoute
+  "/room/$roomId": typeof RoomRoomIdRoute
 }
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '/room/$roomId': typeof RoomRoomIdRoute
+  "/": typeof IndexRoute
+  "/room/$roomId": typeof RoomRoomIdRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
-  '/': typeof IndexRoute
-  '/room/$roomId': typeof RoomRoomIdRoute
+  "/": typeof IndexRoute
+  "/room/$roomId": typeof RoomRoomIdRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/room/$roomId'
+  fullPaths: "/" | "/room/$roomId"
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/room/$roomId'
-  id: '__root__' | '/' | '/room/$roomId'
+  to: "/" | "/room/$roomId"
+  id: "__root__" | "/" | "/room/$roomId"
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
@@ -49,19 +49,19 @@ export interface RootRouteChildren {
   RoomRoomIdRoute: typeof RoomRoomIdRoute
 }
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
+    "/": {
+      id: "/"
+      path: "/"
+      fullPath: "/"
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/room/$roomId': {
-      id: '/room/$roomId'
-      path: '/room/$roomId'
-      fullPath: '/room/$roomId'
+    "/room/$roomId": {
+      id: "/room/$roomId"
+      path: "/room/$roomId"
+      fullPath: "/room/$roomId"
       preLoaderRoute: typeof RoomRoomIdRouteImport
       parentRoute: typeof rootRouteImport
     }
